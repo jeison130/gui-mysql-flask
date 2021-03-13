bbffaa1d3814cc20421ad91e50f9acfc0ff82e89
Jeyson Calvache <jeison130@gmail.com>
2021-03-12 21:28:59 -0500

    MVC

--- src/connection_bd/bd.py
+++ src/connection_bd/bd.py
@@ -4,10 +4,10 @@ from flask import Flask
 connection = Flask(__name__)
 
 connection.config['MYSQL_DATABASE_HOST'] = 'localhost'
-connection.config['MYSQL_DATABASE_PORT'] = 3306
+connection.config['MYSQL_DATABASE_PORT'] = 3308
 connection.config['MYSQL_DATABASE_USER'] = 'root'
-connection.config['MYSQL_DATABASE_PASSWORD'] = 'Jmerazo96*'
-connection.config['MYSQL_DATABASE_DB'] = 'users'
+connection.config['MYSQL_DATABASE_PASSWORD'] = ''
+connection.config['MYSQL_DATABASE_DB'] = 'flask_mvc'
 
 mysql = MySQL(connection)
 
--- src/models/user.py
+++ src/models/user.py
@@ -1,33 +1,48 @@
 from src.connection_bd.bd import mysql
 from flask import request, redirect, flash
+from src.dto.user import UserDTO
 
 class UsersModel():
     def lists(self):
         cursor = mysql.get_db().cursor()
         #cursor = mysql.get_db().cursor()
         cursor.execute('select * from user') 
+
+        fields_name = [field[0] for field in cursor.description]
         data = cursor.fetchall()
+
+        response = []
+
+        for index, d in enumerate(data):
+            response.append({})
+            for index_field, field in enumerate(fields_name):
+                response[index][fields_name[index_field]] = d[index_field]
+
         cursor.close()
         print(data)
-        return data
+        return response
 
-    def create(self, name,lastname,phone,email,password):
+    def create(self, user: UserDTO):
         cursor = mysql.get_db().cursor()
         #cursor = mysql.cursor()
-        cursor.execute('insert into user (name,lastname,phone,email,password) values (% s,% s,% s,% s,% s)', (name,lastname,phone,email,password))
-        flash('User added successfully!')
+        cursor.execute("""insert into user 
+            (name,lastname,phone,email,password) 
+            values (%s,%s,%s,%s,%s)""", (user.name,user.lastname,user.phone,user.email,user.password,))
+
+        mysql.get_db().commit()
         cursor.close()
 
     def update(name, lastname,phone,email,password,id):
         cursor = mysql.get_db().cursor()
         #cursor = mysql.cursor()
-        cursor.execute('update register set name = ?,lastname = ?,phone = ?,email = ?,password = ? WHERE id = ?', (name, lastname,phone,email,password,id))
-        data = cursor.fetchall()
+        cursor.execute('update register set name = ?,lastname = ?,phone = ?,email = ?,password = ? WHERE id = ?', (name, lastname,phone,email,password,id,))
+        mysql.get_db().commit()
         cursor.close()
         print(data)
             
     def delete(id):
         cursor = mysql.get_db().cursor()
         #cursor = mysql.cursor()
-        cursor.execute('delete from register where id = ?', (id))
+        cursor.execute('delete from register where id = %s', (id,))
+        mysql.get_db().commit()
         cursor.close()
